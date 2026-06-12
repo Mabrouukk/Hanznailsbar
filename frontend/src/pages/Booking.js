831b7b31967574c9b0f6a921837685c1704fb996
Mabrouukk <mahmoudghobashy1202@gmail.com>
2026-06-12 18:17:50 +0300

    15% offer

--- frontend/src/pages/Booking.js
+++ frontend/src/pages/Booking.js
@@ -8,11 +8,9 @@ import './Booking.css';
 const API = process.env.REACT_APP_API_URL || 'http://localhost:5001/api';
 
 const ALL_TIME_SLOTS = [
-  '11:00 AM','11:30 AM',
-  '12:00 PM','12:30 PM','1:00 PM','1:30 PM',
-  '2:00 PM','2:30 PM','3:00 PM','3:30 PM',
-  '4:00 PM','4:30 PM','5:00 PM','5:30 PM',
-  '6:00 PM','6:30 PM','7:00 PM','7:30 PM','8:00 PM','8:30 PM','9:00 PM',
+  '11:00 AM','12:30 PM',
+  '2:00 PM','3:30 PM',
+  '5:00 PM','6:30 PM','8:00 PM',
 ];
 
 export default function Booking() {
